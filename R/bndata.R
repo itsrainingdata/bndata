8dc5cbdfc24054dfa1f37cf2dfaa4a84a5fd4eb4
itsrainingdata <bryonsgit@gmail.com>
2016-08-30 19:50:53 -0400

    Initial package commit

---
+++ R/bndata.R
@@ -0,0 +1,10 @@
+#' bndata: Curated Datasets for Bayesian Networks
+#'
+#' bndata is a centralized repository of publicly available datasets
+#' used to construct Bayesian networks. Mainly intended as a centralized
+#' resource for conducting Bayesian network research and for reproducing
+#' existing results.
+#'
+#' @docType package
+#' @name bndata
+NULL
---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+#
+#  zzz.R
+#  bndata
+#
+#  Created by Bryon Aragam (local) on 8/30/16.
+#  Copyright (c) 2014-2016 Bryon Aragam. All rights reserved.
+#
